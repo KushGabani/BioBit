487fef2b72424aef9baa40077a35829cf495f91c
Kush Gabani <kushgabz2687@gmail.com>
2021-04-02 16:56:07 +0530

    deployed cnn model in flask

--- app.py
+++ app.py
@@ -3,29 +3,38 @@ import warnings
 import shutil
 from flask import Flask, url_for, request, render_template
 from werkzeug.utils import secure_filename
+import tensorflow as tf
+import numpy as np
+import cv2
 
 warnings.filterwarnings("ignore")
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET'])
-def home():
-    return render_template('leaf-classification.html')
+categories = ["Cercospora", "Healthy", "Powder Mildew", "Yellow Mosaic"]
 
-@app.route("/predict", methods=['GET', 'POST'])
-def upload():
+
+
+@app.route("/", methods=['GET', 'POST'])
+def home():
     if request.method == "POST":
         print("Reached Here")
         f = request.files['file']
         print("Reached Here")
         file_path = os.path.join(os.path.dirname(__file__), 'uploads', secure_filename(f.filename))
         f.save(file_path)
-        return render_template("leaf-classification.html")
-        # prediction = get_prediction_from_cnn(file_path)
-        # return prediction
+
+        img = cv2.imread(fpath)
+        img = cv2.resize(img, (WIDTH, HEIGHT))
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = np.expand_dims(img, axis = 0)
+        model = tf.keras.models.load_model("./nn_model/model_27-0.1074.hdf5")
+        prediction = model.predict(img)
+        result = categories[np.argmax(prediction)]
+
+        return render_template('leaf-classification.html', result=result)
     else:
-        print("Else here")
-        return "hi"
+        return render_template('leaf-classification.html', result="")
 
 if __name__ == "__main__":
     app.run(debug=True)
