c0fb6cf811a78913a979b930b42c37b428372d88
KushGabani <kushgabz2687@gmail.com>
2021-06-07 10:33:05 +0530

    Controlled + Uncontrolled model added. 100% COMPLETE

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import os
 import warnings
-import shutil
 from flask import Flask, url_for, request, render_template
 from werkzeug.utils import secure_filename
 import tensorflow as tf
@@ -14,25 +13,26 @@ app = Flask(__name__)
 categories = ["Cercospora", "Healthy", "Powder Mildew", "Yellow Mosaic"]
 
 
-
 @app.route("/", methods=['GET', 'POST'])
 def home():
     if request.method == "POST":
         f = request.files['file']
-        file_path = os.path.join(os.path.dirname(__file__), 'uploads', secure_filename(f.filename))
+        file_path = os.path.join(os.path.dirname(
+            __file__), 'uploads', secure_filename(f.filename))
         f.save(file_path)
 
         img = cv2.imread(file_path)
         img = cv2.resize(img, (256, 256))
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        img = np.expand_dims(img, axis = 0)
-        model = tf.keras.models.load_model("./nn_model/model_27-0.1074.hdf5")
+        img = np.expand_dims(img, axis=0)
+        model = tf.keras.models.load_model("./nn_model/combined.hdf5")
         prediction = model.predict(img)
         result = categories[np.argmax(prediction)]
 
         return render_template('leaf-classification.html', result=result)
     else:
         return render_template('leaf-classification.html', result="")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
